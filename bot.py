41a47554bc2a8ef5ca7338d16d3324d3363ad22b
Norman Vo <92652998+hquanvo@users.noreply.github.com>
2025-10-15 21:18:44 -0700

    Add choose command

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix=">", intents=intents)
 
 
 @bot.event
--- cogs/general.py
+++ cogs/general.py
@@ -1,9 +1,16 @@
 from discord.ext import commands
-
+from random import choice
 
 class General(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    async def choose(self, ctx, *args):
+        if (len(args) < 2):
+            await ctx.send("Not enough options to pick from.")
+        else:
+            await ctx.send(f'{choice(args)}')
 
 
 async def setup(bot):
